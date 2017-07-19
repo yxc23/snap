9c0f5b9de712ae3fb0ca2164ac829016dee134ee
testyxc <yue7882154@qq.com>
2017-07-19 14:13:33 +0800

    修改bug的命名--》岳贤昌

---
+++ mall_0224_manager_stu/src/main/java/com/atguigu/mapper/SpuMapper.java
@@ -0,0 +1,12 @@
+package com.atguigu.mapper;
+
+import java.util.HashMap;
+
+import com.atguigu.bean.T_MALL_PRODUCT;
+public interface SpuMapper {
+
+	int insertSpu(T_MALL_PRODUCT spu);
+
+	void insertImages(HashMap<String, Object> map);
+	
+}
